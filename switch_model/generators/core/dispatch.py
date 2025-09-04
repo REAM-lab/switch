ca39c6ac4168b5fbbe275ee115c327b5945c498d
n7gonzalez <n7gonzalez@ucsd.edu>
2025-09-04 16:22:16 -0700

    typo fix

--- switch_model/generators/core/dispatch.py
+++ switch_model/generators/core/dispatch.py
@@ -311,8 +311,9 @@ def define_components(mod):
             for g in m.CCS_EQUIPPED_GENS if g in m.GENS_FOR_ZONE_TPS[z, t]) -
         (sum(m.DispatchProd[h, t] * m.mwh_per_kg_h2[h] * (1000/33.32)
             for (h, g2, tp) in m.ONSITE_PROD_GEN_TPS
-            if tp == t and g2 in m.GENS_FOR_ZONE_TPS[z, t]) if hasattr(m, "ONSITE_PROD_GEN_TPS") else 0)),
-        doc="Net power from grid-tied generation projects.")
+            if tp == t and g2 in m.GENS_FOR_ZONE_TPS[z, t]) if hasattr(m, "ONSITE_PROD_GEN_TPS") else 0),
+        doc="Net power from grid-tied generation projects."
+    )
     mod.Zone_Power_Injections.append('ZoneTotalCentralDispatch')
 
     if using_local_td:
