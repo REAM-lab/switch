0b3fc30804def77dd2f61148dac2fbc4d0a27add
Natalia Gonzalez <nataliagonzalez@REAMs-Mac-Studio.local>
2025-09-10 10:13:43 -0700

    H2_STORAGE_BLD_YRS and H2_STORAGE_PROJECTS set issue

--- switch_model/hydrogen/advanced/h2_storage.py
+++ switch_model/hydrogen/advanced/h2_storage.py
@@ -192,8 +192,13 @@ def define_components(mod):
     up to 10,000 kg of H2 in that period.
     
     """
-    mod.H2_STORAGE_BLD_YRS = Set(dimen=2, input_file="h2_storage.csv")
-    mod.H2_STORAGE_PROJECTS = Set(dimen=1)
+    mod.H2_STORAGE_BLD_YRS = Set(
+        input_file="h2_storage.csv", 
+        dimen=2)
+    mod.H2_STORAGE_PROJECTS = Set(
+        dimen=1,
+        ordered=False,
+        initialize=lambda m: set(s for (s, bld_yr) in m.H2_STORAGE_BLD_YRS))
     mod.h2stor_load_zone = Param(mod.H2_STORAGE_PROJECTS, input_file="h2_storage.csv",
                               within=mod.LOAD_ZONES)
     mod.h2stor_type = Param(mod.H2_STORAGE_PROJECTS, input_file="h2_storage.csv")
@@ -613,13 +618,6 @@ def define_components(mod):
     )
 
 def load_inputs(mod, switch_data, inputs_dir):
-    raw_data = switch_data.data(name='H2_STORAGE_BLD_YRS')
-    # Keep only the first two columns for the set
-    switch_data.data()['H2_STORAGE_BLD_YRS'] = {
-        None: [(row[0], row[1]) for row in raw_data]
-    }
-
-    
     switch_data.load(filename=os.path.join(inputs_dir, "h2_storage.csv"),
                      set=mod.H2_STORAGE_BLD_YRS)
 
