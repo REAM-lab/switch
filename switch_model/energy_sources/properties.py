97db8b734916fdbd0bfe10a6d2ff99940554d818
Natalia Gonzalez <n7gonzalez@ucsd.edu>
2026-02-17 23:06:10 +0000

    fixing bug with NH3 and PM2.5 definition

--- switch_model/energy_sources/properties.py
+++ switch_model/energy_sources/properties.py
@@ -25,8 +25,8 @@ INPUT FILE FORMAT
         energy_source
 
     fuels.csv
-        fuel, co2_intensity, upstream_co2_intensity, nox_intensity, so2_intensity, ch4_intensity
-
+        fuel, co2_intensity, upstream_co2_intensity, nox_intensity, 
+        so2_intensity, ch4_intensity, nh3_intensity, pm25_intensity
 
 """
 
@@ -135,6 +135,10 @@ def define_components(mod):
                                 input_column="so2_intensity", )
     mod.f_ch4_intensity = Param(mod.FUELS, within=NonNegativeReals, default=0, input_file="fuels.csv",
                                 input_column="ch4_intensity", )
+    mod.f_nh3_intensity = Param(mod.FUELS, within=NonNegativeReals, default=0, input_file="fuels.csv",
+                                input_column="nh3_intensity", )
+    mod.f_pm25_intensity = Param(mod.FUELS, within=NonNegativeReals, default=0, input_file="fuels.csv",
+                                input_column="pm25_intensity", )
 
     mod.min_data_check('f_co2_intensity')
     # Ensure that fuel and non-fuel sets have no overlap.
